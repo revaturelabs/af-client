d28e3cfe9176879913aa9ced3c2e80fe1cbe3fa8
Bryan.Wood <bryan.wood@lntinfotech.com>
2021-03-04 17:54:28 -0500

    Initial component creation
    components created, fields still need adding, as well as general tests on the components

--- af-client/src/app/app.module.ts
+++ af-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationComponent } from './location/location.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { BuildingComponent } from './building/building.component';
+import { RoomComponent } from './room/room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocationComponent,
+    ReservationComponent,
+    BuildingComponent,
+    RoomComponent
   ],
   imports: [
     BrowserModule,
