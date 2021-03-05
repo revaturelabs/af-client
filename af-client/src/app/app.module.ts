538f661c7f6b32ee8d072ea30940361d63a10645
unknown <connorcaseyc@gmail.com>
2021-03-05 10:09:24 -0500

    revising module file

--- af-client/src/app/app.module.ts
+++ af-client/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LocationService } from './services/location.service';
-=======
 import { LocationComponent } from './location/location.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { BuildingComponent } from './building/building.component';
 import { RoomComponent } from './room/room.component';
->>>>>>> d28e3cfe9176879913aa9ced3c2e80fe1cbe3fa8
 
 @NgModule({
   declarations: [
