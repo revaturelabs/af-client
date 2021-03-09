77fb4e98eb75e5a48b32ff49949a5bb19df3024b
unknown <connorcaseyc@gmail.com>
2021-03-09 10:42:34 -0500

    fixing location dto

--- af-client/src/app/models/location-dto.ts
+++ af-client/src/app/models/location-dto.ts
@@ -1,5 +1,3 @@
-import { BuildingDto } from "./building-dto";
-
 export class LocationDto {
-    constructor( public locationId: number, state: string, city: string, zipCode: string, buildings: BuildingDto ) {}
+    constructor( public locationId: number, state: string, city: string, zipCode: string, numBuildings: number ) {}
 }
