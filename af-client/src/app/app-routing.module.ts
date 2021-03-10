5dd6c56ede6306bbcc5ef030243db850c83bd29f
SavitaBhor <67609286+SavitaBhor@users.noreply.github.com>
2021-03-10 18:02:21 -0500

    Menu added

--- af-client/src/app/app-routing.module.ts
+++ af-client/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BuildingComponent } from './components/building/building.component';
+import { LocationComponent } from './components/location/location.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
+import { RoomComponent } from './components/room/room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/location', pathMatch: 'full' },
+  {path: 'location', component: LocationComponent},
+  {path: 'building', component: BuildingComponent},
+  {path: 'room', component: RoomComponent},
+  {path: 'reservation', component: ReservationComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
