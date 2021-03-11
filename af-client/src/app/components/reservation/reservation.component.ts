b0be58d5f8e5ba0d24ad3b48aa6e8c932743838d
sherry <zsherry0803@gmail.com>
2021-03-11 14:02:36 -0500

    reservations get by room id showed

--- af-client/src/app/components/reservation/reservation.component.ts
+++ af-client/src/app/components/reservation/reservation.component.ts
@@ -12,7 +12,7 @@ export class ReservationComponent implements OnInit {
 
   @Input() selectedReservation: Reservation;
   response: string;
-
+  roomId: number;
   constructor(private reservationService: ReservationService, private router: Router) {
 
   }
@@ -21,6 +21,14 @@ export class ReservationComponent implements OnInit {
     this.reservationService.getAllReservations()
       .subscribe((reservations: Reservation[]) => {
         console.log('getting reservations');
+        this.reservations = reservations;
+      });
+  }
+  getAllReservationsByRoomId(roomId : number):void {
+    // let roomId = idForm.value.searchId
+    this.reservationService.getAllReservationsByRoomId(roomId)
+      .subscribe((reservations: Reservation[]) => {
+        console.log('getting reservations by room id');
         this.reservations = reservations;
       });
   }
