0102beb1ef349bcbc0f1d63ee44b76fed869e9c0
Joan Marin <johnm7770@gmail.com>
2021-03-12 12:32:55 -0500

    url changes for production

--- af-client/src/app/services/reservation.service.ts
+++ af-client/src/app/services/reservation.service.ts
@@ -11,55 +11,55 @@ export class ReservationService {
   constructor(private httpClient: HttpClient) { }
 
   getReservationById(reservationId: number) {
-    const url = environment.reservartionBackendUrl + `api/reservations/${reservationId}`;
+    const url = environment.reservartionBackendUrl + `${reservationId}`;
 
     return this.httpClient.get<Reservation>(url);
   }
 
   getAllReservations() {
-    const url = environment.reservartionBackendUrl + `api/reservations/`;
+    const url = environment.reservartionBackendUrl;
 
     return this.httpClient.get<Reservation[]>(url);
   }
 
   getAllReservationsByRoomId(roomId: number) {
-    const url = environment.reservartionBackendUrl + `api/reservations/rooms/${roomId}`;
+    const url = environment.reservartionBackendUrl + `rooms/${roomId}`;
 
     return this.httpClient.get<Reservation[]>(url);
   }
 
   addReservation(reservation: Reservation) {
-    const url = environment.reservartionBackendUrl + `api/reservations/`;
+    const url = environment.reservartionBackendUrl;
 
     return this.httpClient.post<Reservation>(url, reservation);
   }
 
   updateReservation(reservation: Reservation) {
-    const url = environment.reservartionBackendUrl + `api/reservations/`;
+    const url = environment.reservartionBackendUrl;
 
     return this.httpClient.put<Reservation>(url, reservation);
   }
 
   deleteReservation(reservationId: number) {
-    const url = environment.reservartionBackendUrl + `api/reservations/${reservationId}`;
+    const url = environment.reservartionBackendUrl + `${reservationId}`;
 
     return this.httpClient.delete(url);
   }
 
   assignBatch( reservation: Reservation, batchId: number ) {
-    const url = environment.reservartionBackendUrl + `api/reservations/${reservation.id}/${batchId}`;
+    const url = environment.reservartionBackendUrl + `${reservation.id}/${batchId}`;
 
     return this.httpClient.put<string>(url, null);
   }
 
   getTrainingStationReservations() {
-    const url = environment.reservartionBackendUrl + `api/reservations/trainingstations`;
+    const url = environment.reservartionBackendUrl + `trainingstations`;
 
     return this.httpClient.get<Reservation[]>(url);
   }
 
   getAllAvailableMeetingRooms(buildingId: number, startDate: string, endDate: string) {
-    const url = environment.reservartionBackendUrl + `api/reservations/${buildingId}/meetingrooms`;
+    const url = environment.reservartionBackendUrl + `${buildingId}/meetingrooms`;
     const requestBody = {
       startDate: startDate,
       endDate: endDate
--- af-client/src/environments/environment.prod.ts
+++ af-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   caliberUrl: "https://caliber2-mock.revaturelabs.com:443/mock/training/",
-  locationBackendUrl: "",
-  reservartionBackendUrl: ""
+  locationBackendUrl: "http://a36454319e2484fb88088f5a9256d7e2-494854720.us-east-1.elb.amazonaws.com/",
+  reservartionBackendUrl: "http://a36454319e2484fb88088f5a9256d7e2-494854720.us-east-1.elb.amazonaws.com/reservation/api/"
 };
