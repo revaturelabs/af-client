5987030b9e8e55b84ef2e9d3d994a9b36fe53699
Joan Marin <johnm7770@gmail.com>
2021-03-12 11:33:12 -0500

    Unit Test Error Removal, Tests should still be changed

--- af-client/src/app/components/reservation/reservation.component.spec.ts
+++ af-client/src/app/components/reservation/reservation.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReservationComponent } from './reservation.component';
+import { RouterTestingModule } from "@angular/router/testing";
+import { FormsModule } from '@angular/forms/';
 
 describe('ReservationComponent', () => {
   let component: ReservationComponent;
@@ -8,9 +10,14 @@ describe('ReservationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReservationComponent ]
+      declarations: [ReservationComponent],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        FormsModule
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
