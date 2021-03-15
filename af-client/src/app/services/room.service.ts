790650bd0a060ea1534aa69861ad852c0cd30900
Ulric Todman <rick.sinclair.nyc@gmail.com>
2021-03-15 10:45:47 -0400

    added service methods. pending additional tests

--- af-client/src/app/services/room.service.ts
+++ af-client/src/app/services/room.service.ts
@@ -2,14 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { RoomDetailsDto } from '../models/room-details-dto';
 import { RoomDto } from '../models/room-dto';
+import { RoomRequestDto } from '../models/room-request-dto';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RoomService {
   constructor(private httpClient: HttpClient) {}
-
+  ilsDto;
   getAllRooms(): Observable<RoomDto[]> {
     const url = environment.locationBackendUrl + '/rooms';
     return this.httpClient.get<RoomDto[]>(url);
@@ -32,5 +34,49 @@ export class RoomService {
   getAllPhysicalTrainingRooms(): Observable<RoomDto[]> {
     const url = environment.locationBackendUrl + '/rooms/physical/training';
     return this.httpClient.get<RoomDto[]>(url);
+  }
+
+  getAllRemoteRooms(): Observable<RoomDto[]> {
+    const url = environment.locationBackendUrl + '/rooms/remote';
+    return this.httpClient.get<RoomDto[]>(url);
+  }
+
+  getAllPhysicalRooms(): Observable<RoomDto[]> {
+    const url = environment.locationBackendUrl + '/rooms/physical';
+    return this.httpClient.get<RoomDto[]>(url);
+  }
+
+  getRoomsByBuildingId(buildingId: number): Observable<RoomDto[]> {
+    const url = environment.locationBackendUrl + `/rooms/${buildingId}`;
+    return this.httpClient.get<RoomDto[]>(url);
+  }
+
+  getAllMeetingRooms(): Observable<RoomDto[]> {
+    const url = environment.locationBackendUrl + '/rooms/meeting';
+    return this.httpClient.get<RoomDto[]>(url);
+  }
+
+  deleteRoom(id: number): Observable<void> {
+    const url = environment.locationBackendUrl + `/room/${id}`;
+    return this.httpClient.delete<void>(url);
+  }
+
+  updateRoom(id: number, reqObj: RoomRequestDto): Observable<void> {
+    const url = environment.locationBackendUrl + `/room/${id}`;
+    return this.httpClient.put<void>(url, reqObj);
+  }
+
+  createRoom(
+    reqObj: RoomRequestDto,
+    buildingId: number
+  ): Observable<RoomDetailsDto> {
+    const url =
+      environment.locationBackendUrl + `/buildings/${buildingId}/room`;
+    return this.httpClient.put<RoomDetailsDto>(url, reqObj);
+  }
+
+  getRoom(id: number): Observable<RoomDetailsDto> {
+    const url = environment.locationBackendUrl + `/room/${id}`;
+    return this.httpClient.get<RoomDetailsDto>(url);
   }
 }
