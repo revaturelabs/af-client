9c7e5b9b848530d8ee575d24ba58d8b43c736235
lip <phillip@lipco.dev>
2021-04-07 08:01:06 -0700

    removed env

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+    production: true,
+    API_URI: "secret",
+    // firebase web browser key for production
+    API_KEY: "AIzaSyBUYFPS95UhHJ6JknljS0R_7iTg0Jq22xQ"
+};
