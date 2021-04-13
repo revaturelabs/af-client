06504c126bbf4fb89ec2c657a59d5387de0aed65
Troy Houston <35402866+troyhou7@users.noreply.github.com>
2021-04-13 18:48:18 -0400

    Fixed Trainer Dashboard to show Reservations. Does not filter by 'Cancelled' yet. Cancel button works now. Had to make a lot of backend calls to get Location/Building/Room info for each reservation.

--- src/app/services/reservation/reservation.service.ts
+++ src/app/services/reservation/reservation.service.ts
@@ -100,7 +100,6 @@ export class ReservationService {
 
   // Read
   getReservationsByRoom(room: Room): Observable<Reservation[]> {
-    console.log(room.roomId);
     return this.http
       .get<Reservation[]>(
         `${this.BASE_URL}/reservations?roomId=${room.roomId}`,this.options
@@ -114,11 +113,16 @@ export class ReservationService {
   }
 
   getReservationByReserver(): Observable<Reservation[]> {
-    return of(this.reservations).pipe(
-      tap(() => this.loadingBar.start()),
-      delay(1000),
-      tap(() => this.loadingBar.complete())
-    );
+    return this.http
+      .get<Reservation[]>(
+        `${this.BASE_URL}/reservations?reserver=${this.authService.decodedJwtDTO?.email}`,this.options
+      );
+    
+    // return of(this.reservations).pipe(
+    //   tap(() => this.loadingBar.start()),
+    //   delay(1000),
+    //   tap(() => this.loadingBar.complete())
+    // );
   }
 
   // Update
