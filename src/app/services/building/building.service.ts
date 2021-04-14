0bb115a275bd6125ebb2cb6e56cf6268f3d972f7
XianbinZhen <62436049+XianbinZhen@users.noreply.github.com>
2021-04-14 10:47:31 -0400

    removed unuse code from location services

--- src/app/services/building/building.service.ts
+++ src/app/services/building/building.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable} from 'rxjs';
 import { Building } from 'src/app/models/building';
-import { delay } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { LocationService } from '../location/location.service';
@@ -18,60 +17,23 @@ export class BuildingService {
     }
   }
 
-  buildings: Building[] = [
-    { buildingId: 10, address: 'address 0', locationId: 1 },
-    { buildingId: 1, address: 'address 1', locationId: 1 },
-    { buildingId: 2, address: 'address 2', locationId: 2 },
-    { buildingId: 3, address: 'address 3', locationId: 1 },
-    { buildingId: 4, address: 'address 4', locationId: 1 },
-    { buildingId: 5, address: 'address 5', locationId: 2 },
-    { buildingId: 6, address: 'address 6', locationId: 3 },
-    { buildingId: 10, address: 'address 0', locationId: 1 },
-    { buildingId: 1, address: 'address 1', locationId: 1 },
-    { buildingId: 2, address: 'address 2', locationId: 2 },
-    { buildingId: 3, address: 'address 3', locationId: 1 },
-    { buildingId: 4, address: 'address 4', locationId: 1 },
-    { buildingId: 5, address: 'address 5', locationId: 2 },
-    { buildingId: 6, address: 'address 6', locationId: 3 },
-    { buildingId: 10, address: 'address 0', locationId: 1 },
-    { buildingId: 1, address: 'address 1', locationId: 1 },
-    { buildingId: 2, address: 'address 2', locationId: 2 },
-    { buildingId: 3, address: 'address 3', locationId: 1 },
-    { buildingId: 4, address: 'address 4', locationId: 1 },
-    { buildingId: 5, address: 'address 5', locationId: 2 },
-    { buildingId: 6, address: 'address 6', locationId: 3 },
-    { buildingId: 10, address: 'address 0', locationId: 1 },
-    { buildingId: 1, address: 'address 1', locationId: 1 },
-    { buildingId: 2, address: 'address 2', locationId: 2 },
-    { buildingId: 3, address: 'address 3', locationId: 1 },
-    { buildingId: 4, address: 'address 4', locationId: 1 },
-    { buildingId: 5, address: 'address 5', locationId: 2 },
-    { buildingId: 6, address: 'address 6', locationId: 3 },
-  ];
-
   constructor(private locationService:LocationService,private authService: AuthService, private httpClient: HttpClient) {}
 
 
   getBuildingsByLocationId(locationId: number): Observable<Building[]> {
-    //return of(this.buildings.filter((e) => e.locationId == locationId)).pipe(delay(1000));
-
-    return this.httpClient.get<Building[]>(`${this.baseUrl}/locations/${this.locationService.currentLocation?.locationId}/buildings`, this.options);
+    return this.httpClient.get<Building[]>(`${this.baseUrl}/locations/${locationId}/buildings`, this.options);
   }
 
   createBuilding(building: Building): Observable<Building> {
     return this.httpClient.post<Building>(`${this.baseUrl}/locations/${building.locationId}/buildings`,building, this.options);
-    this.buildings.push(building);
-    return of(building).pipe(delay(1000));
   }
 
   updateBuilding(building: Building): Observable<Building> {
     return this.httpClient.put<Building>(`${this.baseUrl}/locations/${building.locationId}/buildings/${building.buildingId}`,building, this.options);
-    return of(building).pipe(delay(1000));
   }
 
   deleteBuildingById(building: Building): Observable<boolean> {
     return this.httpClient.delete<boolean>(`${this.baseUrl}/locations/${building.locationId}/buildings/${building.buildingId}`, this.options);
-    return of(false).pipe(delay(1000));
   }
 
 }
--- src/app/services/location/location.service.ts
+++ src/app/services/location/location.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-//import { delay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Location } from 'src/app/models/location';
 import { AuthService } from '../auth/auth.service';
 
@@ -9,40 +8,46 @@ import { AuthService } from '../auth/auth.service';
   providedIn: 'root',
 })
 export class LocationService {
-
-
   currentLocation?: Location = {};
   baseUrl = 'http://104.154.225.237:80';
   options = {
     headers: {
-      Authorization: this.authService.jwt!
-    }
-  }
+      Authorization: this.authService.jwt!,
+    },
+  };
 
-  constructor(private authService: AuthService, private httpClient: HttpClient) {}
+  constructor(
+    private authService: AuthService,
+    private httpClient: HttpClient
+  ) {}
 
   getAllLocation(): Observable<Location[]> {
-    //return of(this.locations).pipe(delay(1500));
-    return this.httpClient.get<Location[]>(`${this.baseUrl}/locations`, this.options);
+    return this.httpClient.get<Location[]>(
+      `${this.baseUrl}/locations`,
+      this.options
+    );
   }
 
   deleteLocation(localtion: Location): Observable<boolean> {
-    // return of(false).pipe(delay(1500));
-    return this.httpClient.delete<boolean>(`${this.baseUrl}/locations/${localtion.locationId}`, this.options);
-
+    return this.httpClient.delete<boolean>(
+      `${this.baseUrl}/locations/${localtion.locationId}`,
+      this.options
+    );
   }
 
   createLocation(location: Location): Observable<Location> {
-    // this.locations.push(location);
-    // return of(location).pipe(delay(1500));
-    return this.httpClient.post<Location>(`${this.baseUrl}/locations`, location, this.options);
-
+    return this.httpClient.post<Location>(
+      `${this.baseUrl}/locations`,
+      location,
+      this.options
+    );
   }
 
   updateLocation(location: Location): Observable<Location> {
-    // return of(location).pipe(delay(1500));
-    return this.httpClient.put<Location>(`${this.baseUrl}/locations/${location.locationId}`, location, this.options);
-
+    return this.httpClient.put<Location>(
+      `${this.baseUrl}/locations/${location.locationId}`,
+      location,
+      this.options
+    );
   }
-
 }
